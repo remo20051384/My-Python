9060c560ebdeed49fa34796233c9ed514d2276ef
remo20051384 <rezamoghaddam834@gmail.com>
2024-07-31 17:17:09 +0330

    Add files via upload

---
+++ E2.py
@@ -0,0 +1,18 @@
+R1 = int(input('Please enter the R1\n'))
+Pi = 3
+
+P = 2 * Pi * R1
+S = Pi * (R1**2)
+
+print(f'P = {P}')
+print(f'S = {S}')
+
+R2 = int(input('Please enter the R2\n'))
+
+P = 2 * Pi * R2
+S = Pi * (R2**2)
+
+print(f'P = {P}')
+print(f'S = {S}')
+
+
